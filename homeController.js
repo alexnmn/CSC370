7a80015e1702f9c76ff33153b41f36a64e04985f
Alex <alexander.neiman@antsquare.com>
2016-07-10 20:39:42 -0700

    Make Table appear on page

--- homeController.js
+++ homeController.js
@@ -1,21 +1,22 @@
 function getTable(type,str) {
-		    if (str == "") {
-		        document.getElementById("txtHint").innerHTML = "";
-		        return;
-		    } else { 
-		        if (window.XMLHttpRequest) {
-		            // code for IE7+, Firefox, Chrome, Opera, Safari
-		            xmlhttp = new XMLHttpRequest();
-		        } else {
-		            // code for IE6, IE5
-		            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-		        }
-		        xmlhttp.onreadystatechange = function() {
-		            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-		                document.getElementById("txtHint").innerHTML = xmlhttp.responseText;
-		            }
-		        };
-		        xmlhttp.open("GET","generateTable.php?t="+type+"&q="+str,true);
-		        xmlhttp.send();
-		    }
+	if (str == "") {
+		return;
+	}else { 
+		if (window.XMLHttpRequest) {
+			// code for IE7+, Firefox, Chrome, Opera, Safari
+			xmlhttp = new XMLHttpRequest();
+		} else {
+			// code for IE6, IE5
+			xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 		}
+		xmlhttp.onreadystatechange = function() {
+			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+				console.log("freedom");
+				document.getElementById("txtHint").innerHTML = xmlhttp.responseText;
+			}
+		};
+		var x = "generateTable.php?t=".concat(type.concat("&q=".concat(str)));
+		xmlhttp.open("GET",x,true);
+		xmlhttp.send();
+	}
+}
